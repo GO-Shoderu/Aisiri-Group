c13b5571a05f8c7af3fc443a60548222a656cc3a
GO-Shoderu <go.shoderu@gmail.com>
2025-05-14 12:40:24 +0200

    Restructured the navigation pane

--- view/src/pages/constant/Nav/Nav.js
+++ view/src/pages/constant/Nav/Nav.js
@@ -82,42 +82,35 @@ const Nav = () => {
               </Link>
             </li>
             <li className="nav-item mx-4">
-              <Link
-                to="/profile"
-                onClick={() => handleNavLinkClick("/profile")}
+              <a
+                href="https://shop.aisirigroup.co.za/?page_id=9"
+                className="nav-link company"
+                target="_self"
+                rel="noopener noreferrer"
+              >
+                Refund Policy
+              </a>
+            </li>
+
+            <li className="nav-item mx-4">
+              <a
+                href="https://shop.aisirigroup.co.za/"
+                className="nav-link"
+                target="_self"
+                rel="noopener noreferrer"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
-                  className={` mt-2 pt-1 ${
-                    activeLink === "/profile"
-                      ? "profile-clicked"
-                      : "profile-nClicked"
-                  }`}
+                  className="mt cart-nClicked"
                   width="17px"
                   height="17px"
                 >
-                  {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
-                  <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
-                </svg>
-              </Link>
-            </li>
-            <li className="nav-item mx-4">
-              <Link to="/cart" onClick={() => handleNavLinkClick("/cart")}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                  className={` mt-2 pt-1 ${
-                    activeLink === "/cart" ? "cart-clicked" : "cart-nClicked"
-                  }`}
-                  width="17px"
-                  height="17px"
-                >
-                  {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
                   <path d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
                 </svg>
-              </Link>
+              </a>
             </li>
+
           </ul>
         </div>
       </div>
