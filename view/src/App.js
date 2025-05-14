b0dd0dd41d32f6a00f48280f22b592e1c93cfaa6
GO-Shoderu <go.shoderu@gmail.com>
2025-05-14 12:36:27 +0200

    Integrated the newly created component to the App component

--- view/src/App.js
+++ view/src/App.js
@@ -1,31 +1,38 @@
+// import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+
+// import Nav from "./pages/constant/Nav/Nav";
+// import Home from "./pages/Home/Home";
+
+// // import "App.css";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <div className="bg-dark pb-3">
+//         <Nav />
+//         <Home />
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import Nav from "./pages/constant/Nav/Nav";
 import Home from "./pages/Home/Home";
-
-// import "App.css";
+import AgeGate from "./pages/Home/AgeGate/AgeGate";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="bg-dark pb-3">
+        <AgeGate />
         <Nav />
         <Home />
-        {/* <Routes> */}
-          {/* <Route exact path="/" element={<Home />} /> */}
-          {/* <Route path="/about" element={<AboutUS />} />
-          <Route path="/service" element={<Services />} />
-          <Route path="/teams" element={<Teams />} />
-          <Route path="/okumbe" element={<Okumbe />} />
-          <Route path="/matheatsie" element={<Matheatsie />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/ecard" element={<ECard />} /> */}
-        {/* </Routes> */}
-        {/* <Scroll /> */}
-        {/* <Banner /> */}
-        {/* <Footer /> */}
       </div>
     </BrowserRouter>
   );
